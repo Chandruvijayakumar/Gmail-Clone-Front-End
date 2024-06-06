b8e00d18fb10aa90c41c7f0ded1966eead31e0eb
CHANDRU VIJAYAKUMAR <chandru4395393@gmail.com>
2024-06-06 11:15:14 +0530

    First Commit

---
+++ gmail-clone-frontend-main/src/firebase/setup.js
@@ -0,0 +1,14 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB8ZHBfi0dz8VIyuOz9ksKWiAFEywhWhGY",
+//   authDomain: "clone1-f4a39.firebaseapp.com",
+//   projectId: "clone1-f4a39",
+//   storageBucket: "clone1-f4a39.appspot.com",
+//   messagingSenderId: "210125140697",
+//   appId: "1:210125140697:web:c6369dd05f9416a78544c8",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const googleProvider =new GoogleAuthProvider(app);
