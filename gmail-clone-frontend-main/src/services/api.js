c53be9cbbb5fa9d356efbf0ac553c7cb7010c6e0
chandru vijayakumar <chandru4395393@gmail.com>
2024-10-02 12:05:16 +0530

    Last COmmit

--- gmail-clone-frontend-main/src/services/api.js
+++ gmail-clone-frontend-main/src/services/api.js
@@ -1,8 +1,8 @@
 // api.js
 import axios from "axios";
 
-// Use the API URL from the environment variable
-const API_URL = process.env.REACT_APP_API_URL;
+// Use the Render deployment link
+const API_URL = "https://gmail-clone-back-end-hdsg.onrender.com";
 
 const API_GMAIL = async (urlObject, payload, type) => {
   return await axios({
